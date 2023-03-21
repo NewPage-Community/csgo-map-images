637c342c5212ceaf6618465c4db83abfa14bd077
Gunslinger <1007708137@qq.com>
2023-03-21 11:31:08 +0800

    sync: image from Vauff

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # CSGO Map Images
 
-this repository fork from https://github.com/KZGlobalTeam/map-images.
+This repository fork from https://github.com/KZGlobalTeam/map-images.
 
 Images for global CSGO maps available in the following:
 - Full (1920x1080) - JPG/WEBP
@@ -11,8 +11,8 @@ Images in the [source directory](./images) are used to build the variants, high-
 
 ## Usage
 
-Generated images are in the [public](https://github.com/KZGlobalTeam/map-images/tree/public) branch.  
-This repository is also hosted on [GitHub Pages](https://kzglobalteam.github.io/map-images/).
+Generated images are in the [public](https://github.com/NewPage-Community/csgo-map-images/tree/public) branch.  
+This repository is also hosted on [GitHub Pages](https://newpage-community.github.io/csgo-map-images/).
 
 The following are available:
 - `maps.json` and `maps.min.json` - A JSON file containing all the images and their urls.
@@ -26,18 +26,22 @@ The following are available:
 ## Examples
 
 #### I want to use full- sized kz_wetbricks in WEBP format
-- https://github.com/KZGlobalTeam/map-images/raw/public/webp/kz_wetbricks.webp
+- https://github.com/NewPage-Community/csgo-map-images/raw/public/webp/kz_wetbricks.webp
 
 #### I want to use medium- sized bkz_apricity_v3 in JPG format
-- https://github.com/KZGlobalTeam/map-images/raw/public/mediums/bkz_apricity_v3.jpg
+- https://github.com/NewPage-Community/csgo-map-images/raw/public/mediums/bkz_apricity_v3.jpg
 
 #### I want to retrieve all the map images and their urls as JSON
-- https://github.com/KZGlobalTeam/map-images/raw/public/maps.json
-- https://github.com/KZGlobalTeam/map-images/raw/public/maps.min.json
+- https://github.com/NewPage-Community/csgo-map-images/raw/public/maps.json
+- https://github.com/NewPage-Community/csgo-map-images/raw/public/maps.min.json
 
 ## Contributing
 If you would like to add missing map images, follow the steps:
-1. [Fork this repository](https://github.com/KZGlobalTeam/map-images/fork).
+1. [Fork this repository](https://github.com/NewPage-Community/csgo-map-images/fork).
 2. Upload the images to the `images` directory in your repository.
 3. Commit and push your changes.
 4. Create a pull request from your repository to here.
+
+# Thanks
+Most images is from Vauff.
+The sync scripts make depend on thier work. [Maunz-Discord](https://github.com/Vauff/Maunz-Discord)
---
+++ scripts/syncvauff.py
@@ -0,0 +1,83 @@
+import json
+from multiprocessing.pool import ThreadPool
+from PIL import Image
+import urllib.request
+import os
+
+# Get map list
+vauffMapList = {}
+# Get json from url
+with urllib.request.urlopen("https://vauff.com/mapimgs/list.php") as url:
+    data = json.loads(url.read().decode())
+    vauffMapList = data
+
+print('Loaded vauff map list')
+
+def LevenshteinDistance(string1, string2):
+    matrix = []
+
+    for i in range(len(string1) + 1):
+        matrix.append([i])
+
+    for j in range(1, len(string2) + 1):
+        matrix[0].append(j)
+
+    for i in range(1, len(string1) + 1):
+        for j in range(1, len(string2) + 1):
+            if string1[i - 1] == string2[j - 1]:
+                matrix[i].append(matrix[i - 1][j - 1])
+            else:
+                matrix[i].append(min(matrix[i - 1][j], matrix[i][j - 1], matrix[i - 1][j - 1]) + 1)
+
+    return matrix[len(string1)][len(string2)]
+
+def getVauffMapImageURL(map, appId):
+    trimmedMap = map[:31]
+    bestMatch = ''
+    bmLevenshteinDist = 999
+
+    # Loop through all map names and find the one that is the closest match
+    for mapImageName in vauffMapList[str(appId)]:
+        mapImageNameLower = mapImageName.lower()
+        distance = LevenshteinDistance(trimmedMap, mapImageNameLower)
+
+        if distance < bmLevenshteinDist and trimmedMap.startswith(mapImageNameLower):
+            bestMatch = mapImageName
+            bmLevenshteinDist = distance
+
+    if bestMatch == '':
+        return ''
+    else:
+        return 'https://vauff.com/mapimgs/' + str(appId) + '/' + bestMatch.replace(' ', '%20') + '.jpg'
+
+# Get image from url
+def getImage(map):
+    url = getVauffMapImageURL(map, 730)
+    # Check url empty
+    if (url == ''): return
+    response = urllib.request.urlopen(url)
+    image = Image.open(response)
+    # Get image resolution from stream
+    width, height = image.size
+    if (width != 1920 or height != 1080):
+        print(f"{map} image is not 1920x1080")
+        return
+    # Save image to file
+    print(f'Saving {map}...')
+    image.save(f"../images/{map}.jpg")
+
+# Init Thead pool
+pool = ThreadPool(10)
+
+# Read map & match the image
+mapList = []
+with open('mapcycle.txt', 'r') as f:
+    list = f.readlines()
+    for map in list:
+        map = map.strip()
+        # Check file exists
+        if (os.path.exists(f'../images/{map}.jpg')): continue
+        mapList.append(map)
+
+# Join get image pool
+pool.map(getImage, mapList)
